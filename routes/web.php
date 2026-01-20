cbcdb866f39db39f7692e8b2c716f0bf44c0d31a
Ntic work <ntic.work@gmail.com>
2026-01-20 13:46:04 +0100

    update routes

--- routes/web.php
+++ routes/web.php
@@ -47,13 +47,13 @@ Route::get('/hello', HelloWorldController::class);
 
 /*************Routes Controller Resource************/
 /**
-** Route::get('/produits', 'RproductController@index')->name('index');    //    Appel : <a href="{{ route('name') }}">
-** Route::get('/produits/create',[RproductController::class,'create'])->name('create');
-** Route::post('/produits', [RproductController::class,'store'])->name('store');
-** Route::get('/produits/{id}', [RproductController::class,'show'])->name('show');
-** Route::get('/produits/{id}/edit', [RproductController::class,'edit'])->name('edit'); // Appel : route('edit', ['id' => $id]);
-** Route::put('/produits/{id}', [RproductController::class,'update'])->name('update');
-** Route::delete('/produits/{id}', [RproductController::class,'destroy'])->name('destroy');
+** Route::get('/produits', 'RproductController@index')->name('produits.index');    //    Appel : <a href="{{ route('name') }}">
+** Route::get('/produits/create',[RproductController::class,'create'])->name('produits.create');
+** Route::post('/produits', [RproductController::class,'store'])->name('produits.store');
+** Route::get('/produits/{id}', [RproductController::class,'show'])->name('produits.show');
+** Route::get('/produits/{id}/edit', [RproductController::class,'edit'])->name('produits.edit'); // Appel : route('edit', ['id' => $id]);
+** Route::put('/produits/{id}', [RproductController::class,'update'])->name('produits.update');
+** Route::delete('/produits/{id}', [RproductController::class,'destroy'])->name('produits.destroy');
 **/
 
 Route::resource('produits', RproductController::class);
